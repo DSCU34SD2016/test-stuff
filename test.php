d9282b99f095b685b3be3816af9a811018a3c6b5
dscmawalker <walker.michael.a@edumail.vic.gov.au>
2016-02-09 21:25:56 +1100

    Create test.php

---
+++ test.php
@@ -0,0 +1,18 @@
+<html>
+<head>
+<title>Test PHP page</title>
+</head>
+<body>
+<h1>Form for Stuff</h1>
+<form name="testy">
+<p>Name:<input type=text length=40 name="userName"></input>
+</form>
+<?php
+class testUser{
+public testUser(){
+  $name = "";
+}
+}
+?>
+</body>
+</html>
